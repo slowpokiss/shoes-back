2a280fb45ed51450c227e2a78ef7fa4ad65e9253
Michel <hltvexe@mail.ru>
2025-02-04 00:18:36 +0300

    fixed description of sending response

--- server.js
+++ server.js
@@ -1,132 +1,154 @@
-  const express = require('express');
-  const { Client } = require('pg');
-  const cors = require('cors');
-  require('dotenv').config();
+const express = require("express");
+const { Client } = require("pg");
+const cors = require("cors");
+require("dotenv").config();
 
-  const app = express();
-  const port = process.env.PORT || 3000;
-  app.use(cors());
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(cors());
 
-  // hardСode
-  const moreCount = 6;
-  const topSaleIds = [66, 65, 73];
-  const topSaleMapped = topSaleIds.map((_, index) => `$${index + 1}`).join(', ');
+// hardСode
+const moreCount = 6;
+const topSaleIds = [66, 65, 73];
+const topSaleMapped = topSaleIds.map((_, index) => `$${index + 1}`).join(", ");
 
-  const client = new Client({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    ssl: { rejectUnauthorized: false }
-  });
+const client = new Client({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: { rejectUnauthorized: false },
+});
 
-  client.connect()
-    .then(() => console.log('successfully connected!'))
-    .catch((err) => console.error('error connection', err.stack));
+client
+  .connect()
+  .then(() => console.log("successfully connected!"))
+  .catch((err) => console.error("error connection", err.stack));
 
+app.get("/api/categories", async (req, res) => {
+  try {
+    const result = await client.query("SELECT * FROM public.categories");
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ description: "No categories data in database", error: err });
+  }
+});
 
-  app.get('/api/categories', async (req, res) => {
-    try {
-      const result = await client.query('SELECT * FROM public.categories');
-      res.json(result.rows);
-    } catch (err) {
-      console.error(err);
-      res.status(500).send('No categories data in database', err);
-    }
-  });
+app.get("/api/top-sales", async (req, res) => {
+  try {
+    const result = await client.query(`SELECT * FROM products WHERE id IN $1`, [
+      topSaleMapped,
+    ]);
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res
+      .status(500)
+      .send({ description: "No top-sales data in database", error: err });
+  }
+});
 
-  app.get('/api/top-sales', async (req, res) => {
-    try {
-      const result = await client.query(`SELECT * FROM products WHERE id IN ${topSaleMapped})`)
-      res.json(result.rows);
-    } catch (err) {
-      console.error(err);
-      res.status(500).send('No top-sales data in database', err);
-    }
-  })
+app.get("/api/products", async (req, res) => {
+  try {
+    const { categoryId = 0, offset = 0, q = "" } = req.query;
 
-  app.get('/api/products', async (req, res) => {
-    try {
-      const { categoryId = 0, offset = 0, q = '' } = req.query;
+    const safeCategoryId = Number(categoryId) || 13;
+    const safeOffset = Number(offset) || 0;
+    const safeLimit = 6;
 
-      const safeCategoryId = Number(categoryId) || 13;
-      const safeOffset = Number(offset) || 0;
-      const safeLimit = 6;
+    let queryText = `SELECT * FROM products WHERE category_id = ${safeCategoryId}`;
 
-      let queryText = `SELECT * FROM products WHERE category_id = ${safeCategoryId}`;
-
-      if (q && q.trim()) {
-        const searchTerm = `%${q.trim().toLowerCase()}%`;
-        queryText += ` AND (LOWER(title) LIKE '${searchTerm}' OR LOWER(color) = '${q.trim().toLowerCase()}')`;
-      }
-
-      queryText += ` LIMIT ${safeLimit} OFFSET ${safeOffset}`;
-
-      const { rows } = await client.query(queryText);
-      res.json(rows);
-    } catch (err) {
-      console.error(err);
-      res.status(500).send('No products data in database');
-    }
-  });
-
-
-  app.get('/api/products/:id', async (req, res) => {
-    const id = Number(req.params.id);
-
-    try {
-      const result = await client.query(`SELECT * FROM public.products WHERE id = ${id}`);
-      res.json(result.rows);
-    } catch (err) {
-      console.error(err);
-      res.status(500).send('No current product data in database', err);
-    }
-  });
-
-
-  app.post('/api/order', async (req, res) => {
-    const { owner: { phone, address }, items } = req.body;
-
-    if (typeof phone !== 'string') {
-      return res.status(400).json({ message: 'bad request: phone' });
-    }
-    if (typeof address !== 'string') {
-      return res.status(400).json({ message: 'bad request: address' });
-    }
-    if (!Array.isArray(items)) {
-      return res.status(400).json({ message: 'bad request: items' });
+    if (q && q.trim()) {
+      const searchTerm = `%${q.trim().toLowerCase()}%`;
+      queryText += ` AND (LOWER(title) LIKE '${searchTerm}' OR LOWER(color) = '${q
+        .trim()
+        .toLowerCase()}')`;
     }
 
-    if (!items.every(({ id, price, count }) => 
-      typeof id === 'number' && id > 0 && 
-      typeof price === 'number' && price > 0 && 
-      typeof count === 'number' && count > 0)) {
-      return res.status(400).json({ message: 'bad request: invalid items' });
+    queryText += ` LIMIT ${safeLimit} OFFSET ${safeOffset}`;
+
+    const { rows } = await client.query(queryText);
+    res.json(rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({description: "No products data in database", error: err});
+  }
+});
+
+app.get("/api/products/:id", async (req, res) => {
+  const id = Number(req.params.id);
+
+  try {
+    const result = await client.query(
+      `SELECT * FROM public.products WHERE id = $1`, [id]
+    );
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({description: "No current product data in database", error: err});
+  }
+});
+
+app.post("/api/order", async (req, res) => {
+  const {
+    owner: { phone, address },
+    items,
+  } = req.body;
+
+  if (typeof phone !== "string") {
+    return res.status(400).json({ message: "bad request: phone" });
+  }
+  if (typeof address !== "string") {
+    return res.status(400).json({ message: "bad request: address" });
+  }
+  if (!Array.isArray(items)) {
+    return res.status(400).json({ message: "bad request: items" });
+  }
+
+  if (
+    !items.every(
+      ({ id, price, count }) =>
+        typeof id === "number" &&
+        id > 0 &&
+        typeof price === "number" &&
+        price > 0 &&
+        typeof count === "number" &&
+        count > 0
+    )
+  ) {
+    return res.status(400).send({ description: "bad request: invalid items", error: err });
+  }
+
+  try {
+    const userResult = await client.query(
+      "INSERT INTO users (phone, address) VALUES ($1, $2) RETURNING id",
+      [phone, address]
+    );
+    const userId = userResult.rows[0].id;
+
+    const orderResult = await client.query(
+      "INSERT INTO orders (user_id) VALUES ($1) RETURNING id",
+      [userId]
+    );
+    const orderId = orderResult.rows[0].id;
+
+    for (const { id, price, count } of items) {
+      const totalPrice = price * count;
+      await client.query(
+        "INSERT INTO order_items (order_id, product_id, price, count, total_price) VALUES ($1, $2, $3, $4, $5)",
+        [orderId, id, price, count, totalPrice]
+      );
     }
 
-    try {
-      const userResult = await client.query('INSERT INTO users (phone, address) VALUES ($1, $2) RETURNING id', [phone, address]);
-      const userId = userResult.rows[0].id;
+    return res.status(204).send();
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send({ description: "Server Error", error: err });
+  }
+});
 
-      const orderResult = await client.query('INSERT INTO orders (user_id) VALUES ($1) RETURNING id', [userId]);
-      const orderId = orderResult.rows[0].id;
-
-      for (const { id, price, count } of items) {
-        const totalPrice = price * count;
-        await client.query('INSERT INTO order_items (order_id, product_id, price, count, total_price) VALUES ($1, $2, $3, $4, $5)', 
-          [orderId, id, price, count, totalPrice]);
-      }
-
-      return res.status(204).send();
-
-    } catch (err) {
-      console.error(err);
-      return res.status(500).json({ message: 'Server Error' });
-    }
-  });
-
-
-  app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-  });
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
